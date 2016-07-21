215a4c4e71f71e140ef79c7be1c806712ec7e62e
Juuso Vilmunen <juuso@vilmunen.net>
2016-07-22 00:34:55 +0300

    using 1.5.3

--- scripts/rundev_util.sh
+++ scripts/rundev_util.sh
@@ -6,4 +6,4 @@ if [ ! -z "$ppid" ]; then
 	kill ${ppid}
 fi
 
-java -XX:+HeapDumpOnOutOfMemoryError -Dservice.url=http://192.168.33.20:8080/service -cp target/client-1.5.2-SNAPSHOT-jar-with-dependencies.jar waazdoh.client.app.AppLauncher dev > log.log 2>error.log &
+java -XX:+HeapDumpOnOutOfMemoryError -Dservice.url=http://192.168.33.20:8080/service -cp target/client-1.5.3-SNAPSHOT-jar-with-dependencies.jar waazdoh.client.app.AppLauncher dev > log.log 2>error.log &
--- scripts/runprod_util.sh
+++ scripts/runprod_util.sh
@@ -1,1 +1,1 @@
-java -DWAAZDOH_RUNNER=true -Dservice.url=http://collabthings.org:8088/waazdoh -cp target/client-1.5.2-SNAPSHOT-jar-with-dependencies.jar waazdoh.client.app.AppLauncher prod 
+java -DWAAZDOH_RUNNER=true -Dservice.url=http://collabthings.org:8088/waazdoh -cp target/client-1.5.3-SNAPSHOT-jar-with-dependencies.jar waazdoh.client.app.AppLauncher prod 
